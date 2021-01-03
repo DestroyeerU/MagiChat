4861dba6315902cf4d72a36999acb9b4d074ae8b
DestroyeerU <destroyeeru18@gmail.com>
2021-01-03 12:09:29 -0300

    removed unused file

--- backend/src/app/controllers/ConversationController/utils.ts
+++
@@ -1,4 +0,0 @@
-import { User } from '@entity/user';
-import { ConversationInterface } from '@schemas/Conversation';
-
-export function formatConversationToResponse(conversation: ConversationInterface, user: User) {}
